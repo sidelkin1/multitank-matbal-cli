4657b29d608b237199be35afc4da397471454d50
sidelkin <sidelkin@yandex.ru>
2022-02-24 19:18:32 +0400

    Added project flag on startup julia

--- cli/start.bat
+++ cli/start.bat
@@ -5,5 +5,5 @@ SET tank_params=%datadir%tank_params.csv
 SET tank_prod=%datadir%tank_prod.csv
 SET result_params=%datadir%result_params.csv
 SET result_prod=%datadir%result_prod.csv
-julia main.jl --options options.toml --tank_params %tank_params% --tank_prod %tank_prod% --result_params %result_params% --result_prod %result_prod%
+julia --project=.. main.jl --options options.toml --tank_params %tank_params% --tank_prod %tank_prod% --result_params %result_params% --result_prod %result_prod%
 PAUSE
